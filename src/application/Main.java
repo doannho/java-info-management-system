89aaa31b0ef21f7200186c5b47e3df4fe9b0cffc
doannho <doannho@163.com>
2022-09-11 18:45:53 +0800

    the output can append to the file

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,49 +1,28 @@
 package application;
 
-import java.net.URL;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
-import javax.swing.JFrame;
-
 import javafx.application.Application;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 
 public class Main extends Application{
-	public static FileOutputStream twriter;
-	public static FileOutputStream cwriter;
-	public static void CreateWriter()throws IOException{
-		twriter = new FileOutputStream("TData.txt");
-		cwriter = new FileOutputStream("CData.txt");
-	}
 	public static ArrayList<Teacher>TechList = new ArrayList<>();
 	public static ArrayList<Course>CourList = new ArrayList<>();
 	public static void addTech(String name,String sex,String no) throws IOException {
-		twriter.write((name+' '+sex+' '+no+'\n').getBytes());
+		Files.write(Paths.get("TData.txt"),(name+' '+sex+' '+no+'\n').getBytes(),StandardOpenOption.APPEND);
 		TechList.add(new Teacher(name,sex,no));
 	}
 	public static void addCour(String name,String time,int duration,String techname) throws IOException {
-		cwriter.write((name+' '+time+' '+duration+' '+techname+'\n').getBytes());
+		Files.write(Paths.get("CData.txt"),(name+' '+time+' '+duration+' '+techname+'\n').getBytes(),StandardOpenOption.APPEND);
 		CourList.add(new Course(name,time,duration,techname));
 	}
 	@Override
@@ -61,7 +40,6 @@ public class Main extends Application{
 		System.out.println(s);
 	}
 	public static void main(String[]args) throws IOException {	
-		CreateWriter();
 		launch(args);
 	}
 }
